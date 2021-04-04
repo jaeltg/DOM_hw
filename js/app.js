83d616f6680dfb5afb7c1e0dbffaa0416dc39750
jaeltg <jaeltg@hotmail.com>
2021-04-04 20:32:35 +0100

    attempted delete button and finished css

--- js/app.js
+++ js/app.js
@@ -16,30 +16,53 @@ document.addEventListener('DOMContentLoaded', () => {
     const movieList = document.querySelector('#movie-list');
     movieList.appendChild(movieListItem);
 
+    const listItems = document.querySelectorAll('.list-item')
+    const deleteButton = document.querySelector('#delete-button')
+    listItems.forEach((item) => {
+        deleteButton.addEventListener('click', handleDelete);
+    })
+    
 
     event.target.reset();
   }
 
   const createListItem = (form) => {
       const listItem = document.createElement('li');
+      listItem.classList.add('list-item');
 
       const movieTitleYear = document.createElement('h3');
       movieTitleYear.textContent = `${form.title.value} (${form.year.value})`;
+      movieTitleYear.id = "title-year"
       listItem.appendChild(movieTitleYear);
 
       const movieRunTime = document.createElement('p');
       movieRunTime.textContent = `${form.runningTime.value} min`;
-      listItem.appendChild(movieRunTime);
 
       const movieWatched = document.createElement('p')
       movieWatched.textContent = form.newRewatch.value;
-      listItem.appendChild(movieWatched);
-      
+    
+      const infoContainer = document.createElement('div')
+      infoContainer.appendChild(movieRunTime)
+      infoContainer.appendChild(movieWatched)
+      infoContainer.id = "info-container"
+      listItem.appendChild(infoContainer)
+
+      const deleteButton =  document.createElement('button')
+      deleteButton.textContent = 'Delete';
+      deleteButton.classList.add('button');
+      deleteButton.id = "delete-button";
+      listItem.appendChild(deleteButton);
+    
       return listItem;
   }
 
-  const handleDeleteAll = (event) => {
+  const handleDeleteAll = () => {
       const movieList = document.querySelector('#movie-list');
       movieList.innerHTML = '';
   }
- 
+
+   const handleDelete = (event) => {
+    const movieList = document.querySelector('#movie-list');
+    movieList.removeChild(event.target.parentElement)
+  }
+
